bea9bfffd0a65b5f2bfedae5f920b4219d2efd2d
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-28 21:27:48 +0000

    Hangman

---
+++ Day_7/hangman_words.py
@@ -0,0 +1,9 @@
+word_list = [
+    "abrupto", "absurdo", "abismo", "afijo", "avenida", "banjo", "bayou", "beber", 
+    "blitz", "ventisca", "boggle", "buckaroo", "bufalo", "bufon", "buxom", "zumbido", 
+    "zumbante", "califa", "cobweb", "cockiness", "croquet", "cripta", "curasao", 
+    "ciclo", "dizzying", "duplex", "enanos", "embeber", "equipar", "espionaje", 
+    "exodo", "faking", "fishhook", "fiordo", "fiasco", "flopping", "ponche", 
+    "fucsia", "gossamer", "guino", "haiku", "haphazard", "hipnotizar", "interrogacion", 
+    "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk"
+]
